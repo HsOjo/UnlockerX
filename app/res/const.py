f9fbcfcd20a0f77157551f1064240ad8965fb8b7
HsOjo <1134031392@qq.com>
2019-10-28 22:19:52 +0800

    Release 1.2.1

--- app/res/const.py
+++ app/res/const.py
@@ -2,7 +2,7 @@ class Const:
     author = 'HsOjo'
     app_name = 'UnlockerX'
     app_env = '%s_ENV' % app_name.upper()
-    version = '1.2.0'
+    version = '1.2.1'
     github_page = 'https://github.com/%s/%s' % (author, app_name)
     releases_url = '%s/releases' % github_page
     protector = '[protector]'
