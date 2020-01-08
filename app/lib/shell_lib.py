fbab8c0a9571ec3c36d84bd36f2ba63c831fbe02
HsOjo <1134031392@qq.com>
2020-01-08 09:41:56 +0800

    Update ShellLib.

--- app/lib/shell_lib.py
+++ app/lib/shell_lib.py
@@ -5,8 +5,11 @@ class ShellLib:
     def __init__(self, path):
         self.path = path
 
-    def exec_out(self, *args):
-        return common.execute_get_out([self.path, *args])
+    def exec_out(self, *args, **kwargs):
+        return common.execute_get_out([self.path, *args], **kwargs)
 
-    def exec(self, *args):
-        return common.execute([self.path, *args])
+    def exec(self, *args, **kwargs):
+        return common.execute([self.path, *args], **kwargs)
+
+    def popen(self, *args, **kwargs):
+        return common.popen([self.path, *args], **kwargs)
